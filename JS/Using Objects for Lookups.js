fbc1f19704ef5e3ffc247267954989b719ffd56a
Failj Sagdiev <tczero.70@gmail.com>
2022-11-17 21:59:37 +0300

    Create Using Objects for Lookups.js

---
+++ JS/Using Objects for Lookups.js
@@ -0,0 +1,14 @@
+function phoneticLookup(val) {
+    let result = "";
+    var lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank"
+    };
+    result = lookup[val];
+    return result;
+}
+console.log(phoneticLookup("alpha"));
