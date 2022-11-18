3b187a006b3b7d2ec40ff2dbead21884c2d52039
Failj Sagdiev <tczero.70@gmail.com>
2022-11-18 20:14:42 +0300

    Create Manipulating Complex Objects.js

---
+++ JS/Manipulating Complex Objects.js
@@ -0,0 +1,21 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "Cassette",
+            "LP"
+        ],
+        "gold": true
+    }, {  
+        "artist": "Beau Carnes",
+        "title": "Cereal Man",
+        "release_year": 2003,
+        "formats": [
+            "youtube_video",
+            "LP"
+            ]
+    }
+];
